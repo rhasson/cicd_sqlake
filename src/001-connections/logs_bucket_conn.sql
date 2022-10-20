f73363160bcfa1cd8082fd88229c89d92e7e19d3
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-10-19 21:24:21 -0400

    added s3 connection to logs bucket

--- src/001-connections/logs_bucket_conn.sql
+++ src/001-connections/logs_bucket_conn.sql
@@ -1,5 +1,5 @@
 /* Create a connection to S3 log bucket */
-create s3 connection logs_bucket_conn
+create s3 connection logs_bucket_conn2
     aws_role = 'arn:aws:iam::433987883887:role/royon_ro_role'
     external_id = 'ROYON'
 ;
